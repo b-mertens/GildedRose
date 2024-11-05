72862c15bc5428ee77fbbbb334a5398e820dca7a
Bart Mertens <bmertens@medallia.com>
2024-11-05 22:01:52 +0100

    Add complete fixedList unit test

--- src/GildedRose.Tests/QualityServiceTest.cs
+++ src/GildedRose.Tests/QualityServiceTest.cs
@@ -70,5 +70,44 @@ namespace GildedRose.Test
 
             Assert.AreEqual(items[0].Quality, quality);
         }
+
+        [TestMethod]
+        public void ValidateFixedProgramData()
+        {
+            var items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            new Services().GetService<IQualityService>().UpdateQuality(items);
+
+            //"+5 Dexterity Vest"
+            Assert.AreEqual(items[0].Quality, 19);
+            Assert.AreEqual(items[0].SellIn, 9);
+            //"Aged Brie"
+            Assert.AreEqual(items[1].Quality, 1);
+            Assert.AreEqual(items[1].SellIn, 1);
+            // "Elixir of the Mongoose"
+            Assert.AreEqual(items[2].Quality, 6);
+            Assert.AreEqual(items[2].SellIn, 4);
+            // "Sulfuras, Hand of Ragnaros"
+            Assert.AreEqual(items[3].Quality, 80);
+            Assert.AreEqual(items[3].SellIn, 0);
+            // "Backstage passes to a TAFKAL80ETC concert"
+            Assert.AreEqual(items[4].Quality, 21);
+            Assert.AreEqual(items[4].SellIn, 14);
+            // "Conjured Mana Cake"
+            Assert.AreEqual(items[5].Quality, 5);
+            Assert.AreEqual(items[5].SellIn, 2);
+        }
     }
 }
