6d516220c3f83ef81ba569d77d7cd07d8b243e5a
Bart Mertens <bmertens@medallia.com>
2024-11-05 21:00:23 +0100

    Remove DAL as it seems overkill

--- src/GildedRose.ConsoleCore/Program.cs
+++ src/GildedRose.ConsoleCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GildedRose.Engine;
 using GildedRose.Interfaces.Engine;
+using GildedRose.Models.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
@@ -10,7 +11,21 @@ using var serviceProvider = services.BuildServiceProvider();
 
 System.Console.WriteLine("OMGHAI!");
 
-var items = GildedRose.Data.Items.ItemList;
+var items = new List<Item>
+        {
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 15,
+                Quality = 20
+            },
+            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+        };
+    }  
 
 var qualityService = serviceProvider.GetService<IQualityService>();
 var result = await qualityService.UpdateQualityAsync(items);
