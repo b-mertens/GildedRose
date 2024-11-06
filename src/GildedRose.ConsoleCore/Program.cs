694399c73014bd3dea3deb603eb691bb35940348
Bart Mertens <bmertens@medallia.com>
2024-11-06 19:15:44 +0100

    Use fixed ProductNames in Console

--- src/GildedRose.ConsoleCore/Program.cs
+++ src/GildedRose.ConsoleCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GildedRose.Engine;
 using GildedRose.Interfaces.Engine;
+using GildedRose.Models.General;
 using GildedRose.Models.Models;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,17 +14,17 @@ System.Console.WriteLine("OMGHAI!");
 
 var items = new List<Item>
         {
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item {Name = ProductNames.DexterityVest, SellIn = 10, Quality = 20},
+            new Item {Name = ProductNames.AgedBrie, SellIn = 2, Quality = 0},
+            new Item {Name = ProductNames.ElixirMongoose, SellIn = 5, Quality = 7},
+            new Item {Name = ProductNames.Sulfuras, SellIn = 0, Quality = 80},
             new Item
             {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Name = ProductNames.BackstagePasses,
                 SellIn = 15,
                 Quality = 20
             },
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            new Item {Name = ProductNames.Conjured, SellIn = 3, Quality = 6}
         };
 
 var qualityService = serviceProvider.GetService<IQualityService>();
